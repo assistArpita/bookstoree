faeec1cc70a32f6a710d9dc9ff461994f0c8d6a4
arjun rai <arjun rai@DESKTOP-221UMG0>
2024-06-11 12:42:21 +0530

    commit

---
+++ src/main/java/com/spring/service/BookServic.java
@@ -0,0 +1,31 @@
+package com.spring.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.spring.entity.Book;
+import com.spring.repository.BookRepository;
+
+@Service
+public class BookServic {
+	
+	@Autowired
+	private BookRepository bRepo;
+	
+	public void save(Book b) {
+		bRepo.save(b);
+	}
+	
+	public List<Book> getAllBook(){
+		return bRepo.findAll();
+	}
+	
+	public Book getBookById(int id) {
+		return bRepo.findById(id).get();
+	}
+	public void deleteById(int id) {
+		bRepo.deleteById(id);
+	}
+}
